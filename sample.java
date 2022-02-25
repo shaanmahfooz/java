b19fc7411aade3fa4b4926e26a8016cb452cb22a
shaanmahfooz <98018731+shaanmahfooz@users.noreply.github.com>
2022-02-25 22:49:32 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -1,8 +1,8 @@
-/** 
- * The HelloWorldApp class implements an application that simply prints "Hello World" to standard output.
- */
-public class HelloWorld {
-  public static void main(String[] args) {
-    System.out.printIn("Hello World");
-  }
-}
+class Demo  
+{  
+public static void main(String args[])  
+{  
+System.out.println("Hello!");  
+System.out.println("Java");  
+}  
+}  
