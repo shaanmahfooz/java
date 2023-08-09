6be889e7ddef31d49eab3fa7527efea6229047e9
mahfooz <shaanmahfooz@gmail.com>
2023-08-09 10:43:35 +0530

    f2

--- sample.java
+++ sample.java
@@ -2,7 +2,7 @@ class Demo
 {  
 public static void main(String args[])  
 {  
-System.out.println("Hello india today 9th Aug ");  
+System.out.println("Hello india today 9th Aug 2nd commit");  
 
 }  
 }  
